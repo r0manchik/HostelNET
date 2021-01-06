ee911284e9f74b9d7640fcdbb5af5ea0fa021ccb
theskymaks <theskymaks@gmail.com>
2021-01-06 21:31:10 +0200

    0.0.0.4.1 some fixes

--- HostelNET/Controllers/HomeController.cs
+++ HostelNET/Controllers/HomeController.cs
@@ -1,17 +1,11 @@
 ﻿using HostelNET.Models;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Logging;
-using System;
-using System.Collections.Generic;
 using System.Diagnostics;
-using System.Linq;
-using System.Threading.Tasks;
 
 namespace HostelNET.Controllers
-{     
+{
     public class HomeController : Controller
-
-
     {
         private readonly ILogger<HomeController> _logger;
 
@@ -25,28 +19,44 @@ namespace HostelNET.Controllers
             return View();
         }
 
+        [HttpGet]
         public IActionResult Service()
         {
             return View();
         }
+
+        [HttpPost]
+        public void Service(string name, string email, string option, string content)
+        {
+            var NoSQLcon = new Service_NoSQL() { Name = name, Email = email, Type = option, Description = content };
+            NoSQLcon.Service_InsertInto();
+        }
+
+        [HttpGet]
         public IActionResult Work()
         {
             return View();
         }
+
+        [HttpPost]
+        public void Work(string name, string email, string option, string content)
+        {
+            if (!int.TryParse(option, out int type))
+            {
+                type = 0;
+            }
+            var SQLcon = new Work_SQL() { Name = name, Email = email, Type = type, Description = content };
+            SQLcon.Work_InsertInto();
+        }
+
         public IActionResult Contacts()
         {
             return View();
         }
-        [HttpGet]
+
         public IActionResult Login()
         {
             return View();
-        }
-        [HttpPost]
-        public IActionResult Login(string login, string password)
-        {
-            string authData = $"Login: {login}   Password: {password}";
-            return Content(authData);
         }
 
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
